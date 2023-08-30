62b97760ffd70f53cf83810c43c228914e9e85a2
Matteo Simone Gagliano <matteosg@ext.inditex.com>
2023-08-30 12:41:43 +0200

    Fixed ports

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 4173,
+  },
+  preview: {
+    port: 4173,
+  },
 })
